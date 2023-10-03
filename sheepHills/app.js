49ce97743d664864ff0a139378f495c83062c7b7
sunaliner <dh.kim@hanwha-nxef.com>
2023-10-03 22:59:41 +0900

    feat: 줄에 매달린 상자

--- sheepHills/app.js
+++ sheepHills/app.js
@@ -8,6 +8,8 @@ class App {
         this.ctx = this.canvas.getContext("2d");
         document.body.appendChild(this.canvas);
 
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
         this.hills = [
             new Hill("#fd6bea", 0.2, 12),
             new Hill("#ff59c2", 0.5, 8),
@@ -27,9 +29,9 @@ class App {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight;
 
-        this.canvas.width = this.stageWidth * 2;
-        this.canvas.height = this.stageHeight * 2;
-        this.ctx.scale(2, 2);
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
 
         for (let i = 0; i < this.suns.length; i++) {
             this.suns[i].resize(this.stageWidth, this.stageHeight);
