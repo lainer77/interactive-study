29e8d913e005846ba01a928da3c831ee1b6c4edf
sunaliner <dh.kim@hanwha-nxef.com>
2023-10-03 17:27:25 +0900

    feat: 양 언덕의 각도에 맞춰 이동

--- sheep.js
+++ sheep.js
@@ -40,6 +40,7 @@ export class Sheep {
 
         ctx.save();
         ctx.translate(this.x, this.y);
+        ctx.rotate(closeset.rotation);
         ctx.fillStyle = "#000000";
         // 검은색 박스
         // ctx.fillRect(
@@ -105,9 +106,17 @@ export class Sheep {
     }
 
     getPointOnQuad(x1, y1, x2, y2, x3, y3, t) {
+        const tx = this.quadTangent(x1, x2, x3, t);
+        const ty = this.quadTangent(y1, y2, y3, t);
+        const rotation = -Math.atan2(tx, ty) + (90 * Math.PI) / 180;
         return {
             x: this.getQuadValue(x1, x2, x3, t),
             y: this.getQuadValue(y1, y2, y3, t),
+            rotation,
         };
+    }
+
+    quadTangent(a, b, c, t) {
+        return 2 * (1 - t) * (b - a) + 2 * (c - b) * t;
     }
 }
